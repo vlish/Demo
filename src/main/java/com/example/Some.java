d6dc3c07732050845638bb5c58bf34a85d568249
vlishch <liscynskiy1994>
2016-06-09 18:05:30 +0300

    commit to another branch

--- src/main/java/com/example/Some.java
+++ src/main/java/com/example/Some.java
@@ -6,4 +6,7 @@ import java.util.List;
  * Created by vlishch on 6/8/2016.
  */
 public class Some {
+    public void Some() {
+
+    }
 }
---
+++ src/main/java/com/example/Some2.java
@@ -0,0 +1,7 @@
+package com.example;
+
+/**
+ * Created by vlishch on 6/9/2016.
+ */
+public class Some2 {
+}
